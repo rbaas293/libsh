fc5d2ad395ad464a17fef0fd33766786c4ed49bc
Ryan Baas <rybaas293@gmail.com>
2022-05-10 16:25:54 -0400

    added shell script to download an apt package and all dependencies

---
+++ downloadpkg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PKG=$1
+
+rm -f error.txt
+apt download $(apt-rdepends $PKG | grep -v "^ ") 2> error.txt
+#IF THERE WAS ERRORS (DEPENDENCIES NOT FOUND)
+if [ $(cat error.txt | wc -l) -gt 0 ]
+then
+    partial_command="\("
+    while read -r line
+    do
+        conflictive_package="$(awk '{split($0,array," "); print array[8]}' <<< $line)"
+        partial_command="$partial_command$conflictive_package\|"
+    done < error.txt
+
+    partial_command="$(awk '{print substr($0, 1, length($0)-2)}' <<< $partial_command)\)"
+    eval "apt download \$(apt-rdepends $PKG | grep -v '^ ' | grep -v '^$partial_command$')"
+fi
+rm error.txt
